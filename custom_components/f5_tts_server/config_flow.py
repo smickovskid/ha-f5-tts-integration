8ba9ab44c9556110804fa8f563a96cc0f216a5dd
Damjan Smickovski <smickovskid@outlook.com>
2024-12-20 09:41:43 +0100

    Config flow fixes

--- custom_components/f5_tts_server/config_flow.py
+++ custom_components/f5_tts_server/config_flow.py
@@ -1,4 +1,4 @@
-"""Config flow for F5 TTS Client integration."""
+"""Config flow for Integration F5 TTS."""
 
 from __future__ import annotations
 
@@ -9,6 +9,8 @@ import voluptuous as vol
 
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 from homeassistant.const import CONF_HOST, CONF_PORT
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
 
 from .connection import ConnectionClient
 from .const import DOMAIN
@@ -17,48 +19,91 @@ _LOGGER = logging.getLogger(__name__)
 
 STEP_USER_DATA_SCHEMA = vol.Schema(
     {
-        vol.Required(CONF_HOST, default="127.0.0.1"): str,
-        vol.Required(CONF_PORT, default="9998"): str,
+        vol.Required(CONF_HOST, description={"suggested_value": "127.0.0.1"}): str,
+        vol.Required(CONF_PORT, description={"suggested_value": "9998"}): str,
     }
 )
 
 
-class F5ConfigFlow(ConfigFlow, domain=DOMAIN):
-    """User setup flow via UI."""
+async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
+    """Validate the user input allows us to connect.
+
+    Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
+    """
+    s_client = ConnectionClient(data[CONF_HOST], data[CONF_PORT])
+    await s_client.connect()
+    await s_client.disconnect()
+    return {"title": f"F5 TTS Server - {data[CONF_HOST]}"}
+
+
+class F5TTSConfigFlow(ConfigFlow, domain=DOMAIN):
+    """Handle config flow for F5 TTS"""
 
     VERSION = 1
-    host: str
-    port: str
-    s_client: ConnectionClient
-
-    async def validate_connection(self):
-        """Validate server is up and running."""
-        await self.s_client.connect()
-        await self.s_client.disconnect()
+    _input_data: dict[str, Any]
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Handle the initial step."""
-        errors = {}
+
+        errors: dict[str, str] = {}
+
         if user_input is not None:
-            self.host = user_input[CONF_HOST]
-            self.port = user_input[CONF_PORT]
-            self.s_client = ConnectionClient(self.host, self.port)
-
-            self._async_abort_entries_match(
-                {CONF_HOST: user_input[CONF_HOST], CONF_PORT: user_input[CONF_PORT]}
-            )
-
             try:
-                await self.validate_connection()
-                _LOGGER.info("Successfully set up F5 TTS")
-                return self.async_create_entry(title="F5 TTS Client", data=user_input)
+                info = await validate_input(self.hass, user_input)
             except ConnectionRefusedError:
                 errors["base"] = "cannot_connect"
-            except TimeoutError:
-                errors["base"] = "timeout_connect"
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+
+            if "base" not in errors:
+                await self.async_set_unique_id(info.get("title"))
+                self._abort_if_unique_id_configured()
+                return self.async_create_entry(title=info["title"], data=user_input)
 
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
+
+    async def async_step_reconfigure(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Add reconfigure step to allow to reconfigure a config entry."""
+        errors: dict[str, str] = {}
+        config_entry = self.hass.config_entries.async_get_entry(
+            self.context["entry_id"]
+        )
+
+        if user_input is not None:
+            try:
+                user_input[CONF_HOST] = config_entry.data[CONF_HOST]
+                user_input[CONF_PORT] = config_entry.data[CONF_PORT]
+                await validate_input(self.hass, user_input)
+            except CannotConnect:
+                errors["base"] = "cannot_connect"
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+            else:
+                return self.async_update_reload_and_abort(
+                    config_entry,
+                    unique_id=config_entry.unique_id,
+                    data={**config_entry.data, **user_input},
+                    reason="reconfigure_successful",
+                )
+        return self.async_show_form(
+            step_id="reconfigure",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_HOST, default=config_entry.data[CONF_HOST]): str,
+                    vol.Required(CONF_PORT, default=config_entry.data[CONF_PORT]): str,
+                }
+            ),
+            errors=errors,
+        )
+
+
+class CannotConnect(HomeAssistantError):
+    """Error to indicate we cannot connect."""
